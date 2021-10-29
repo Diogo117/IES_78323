ac58c9cce76799a50dafbeed26b81e63ddf149ea
Diogo <Diogo@DIOGO-PC.lan>
2021-10-29 16:51:53 +0100

    Lab1_2 end

---
+++ IES_78323/Lab1/Lab1_2/src/main/java/com/mycompany/lab1/WeatherStarter.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lab1;
+
+/**
+ *
+ * @author Diogo
+ */
+import java.util.Scanner;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+import java.util.logging.Logger;
+
+/**
+ * demonstrates the use of the IPMA API for weather forecast
+ */
+public class WeatherStarter {
+
+    private static final int CITY_ID_AVEIRO = 1010500;
+    /*
+    loggers provide a better alternative to System.out.println
+    https://rules.sonarsource.com/java/tag/bad-practice/RSPEC-106
+     */
+    private static final Logger logger = Logger.getLogger(WeatherStarter.class.getName());
+
+    public static void  main(String[] args ) {
+
+        /*
+        get a retrofit instance, loaded with the GSon lib to convert JSON into objects
+         */
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.ipma.pt/open-data/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter city Code");
+
+        int cityCode = Integer.parseInt(myObj.nextLine());  // Read user input
+        
+        IpmaService service = retrofit.create(IpmaService.class);
+        Call<IpmaCityForecast> callSync = service.getForecastForACity(cityCode);
+        
+      
+
+        try {
+            Response<IpmaCityForecast> apiResponse = callSync.execute();
+            IpmaCityForecast forecast = apiResponse.body();
+
+            if (forecast != null) {
+                logger.info( "max temp for today: " + forecast.getData().
+                        listIterator().next().getTMax());
+            } else {
+                logger.info( "No results!");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
