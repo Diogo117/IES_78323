15a5a010b3e093c18b35b50aa7d3c3b75cc4dc8f
Diogo <Diogo@DIOGO-PC.lan>
2021-11-04 11:19:28 +0000

    Lab2 exercice 3 Start

---
+++ IES_78323/Lab2/Lab2_3/src/main/java/com/example/Lab2_3/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.Lab2_3;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Lab23Application.class);
+	}
+
+}
---
+++ IES_78323/Lab2/Lab2_3/src/test/java/com/example/Lab2_3/Lab23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lab2_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
